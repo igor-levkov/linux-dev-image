8507f0181caf692a06eca3e15f96fc593d11f551
Igors Levkovs <igor.levkov@gmail.com>
2023-12-19 14:52:30 +0200

    script to min install

---
+++ gnome-desktop-min.sh
@@ -0,0 +1,17 @@
+sudo dnf install \
+   dconf \
+   gdm \
+   gnome-control-center \
+   gnome-initial-setup \
+   gnome-session-wayland-session \
+   gnome-settings-daemon \
+   gnome-shell \
+   gnome-software \
+   kitty \
+   nautilus \
+   polkit
+   gnome-terminal
+#  gnome-boxes \
+#   gnome-connections \
+#   gnome-text-editor \
+#   yelp
